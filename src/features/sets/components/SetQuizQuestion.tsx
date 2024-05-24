37f73630b804c776d85d9a5f8144a030823ced6b
Alex <ac5295@student.jamk.fi>
2024-05-24 12:17:24 +0300

    term question audio button fix

--- src/features/sets/components/SetQuizQuestion.tsx
+++ src/features/sets/components/SetQuizQuestion.tsx
@@ -135,9 +135,11 @@ const SetQuizQuestion: FC<SetQuizQuestionProps> = ({
     <div className="space-y-5 w-full">
       <div className="flex items-center gap-3">
         <p className="font-medium text-muted-foreground">Definition</p>
-        <AudioButton
-          queue={[`https://quizlet.com/${question.term.definitionTtsUrl}`]}
-        />
+        {question.term.definitionTtsUrl && (
+          <AudioButton
+            queue={[`https://quizlet.com/${question.term.definitionTtsUrl}`]}
+          />
+        )}
       </div>
       <TermSide text={question.term.definition} image={question.term.image} />
       <Form {...form}>
